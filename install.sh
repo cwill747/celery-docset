042c9219995c932384090d34b5f20fdd63bed792
Giacomo Comitti <giacomit@gmail.com>
2014-06-09 07:17:05 +0200

    Fix installation script. Install the docset explicitly

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 if [[ -e ./dist/jinja2.docset ]]; then
     mkdir -p "$HOME/Library/Application Support/Dash/DocSets/Jinja2"
     cp -r ./dist/jinja2.docset "$HOME/Library/Application Support/Dash/DocSets/Jinja2"
+    open -a Dash "$HOME/Library/Application Support/Dash/DocSets/Jinja2/jinja2.docset"
     echo -e "Jinja2 docset successfully installed!"
 else
     echo -e "No docset found..."
